7e157a3b6c23c7c0d103f56ed728cddb472fb5a4
bankymono <bankymono@gmail.com>
2021-02-03 20:57:08 +0100

    implemented to do list with drag and drop feature

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ if(todoList == null){
 
 
 
-
-
 console.log(todoList)
 
 function addTodo(){
@@ -33,14 +31,71 @@ function displayTodo(){
     todoNode.innerHTML =``;
 
     for (i = 0; i < todoList.length; ++i){
-        todoNode.innerHTML += `<li>
+        todoNode.innerHTML += `<li draggable=true class="list-item">
                         <div><i class="fas fa-asterisk"></i> ${todoList[i]}</div>
                         <div>
                             <button id="edit" onclick="editTodo(${i})">Edit</button>
-                            <button onclick="deleteTodo(${i})">Delete</button>
+                            <button id="delete" onclick="deleteTodo(${i})">Delete</button>
                         </div>
                      </li>`
     }
+
+    var items = document.querySelectorAll('#todo-list li')
+var dragged = null;
+
+
+for( let i  of  items){
+    i.addEventListener('dragstart', function(){
+        dragged = this;
+        for(it of items){
+            if(it != dragged){
+                it.classList.add('hint')   
+            }
+        }
+    })
+
+    i.addEventListener('dragenter', function(){
+        if(this != dragged){this.classList.add('active')}
+    });
+
+    i.addEventListener('dragleave', function(){
+        this.classList.remove('active')
+    });
+
+    i.addEventListener('dragend', function(){
+        for(it of items){
+            it.classList.remove('hint')
+            it.classList.remove('active')
+        }
+    });
+
+    // i.addEventListener('drag', function(e){
+        
+    // });
+
+    i.addEventListener('dragover', function(e){
+        e.preventDefault()
+    });
+
+    i.addEventListener('drop', function(e){
+        e.preventDefault()
+        if(this != dragged){
+            let all = document.querySelectorAll('#todo-list li'), draggedpos = 0, droppedpos = 0;
+
+            for(let it =0; it < all.length; ++it ){
+                if(dragged == all[it]) draggedpos = it;
+                if(this == all[it]) droppedpos = it;
+            }
+
+            if(draggedpos < droppedpos){
+                this.parentNode.insertBefore(dragged,this.nextSibling);
+            }else{
+                this.parentNode.insertBefore(dragged,this);
+            }
+        }
+    });
+}
+
 }
 
 function deleteTodo(index){
